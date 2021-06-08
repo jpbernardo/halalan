4d7876b21984a708db631df7c4edd084cbfc4e32
Jose Paolo Bernardo <jpbernardo08@gmail.com>
2021-06-08 15:49:10 +0800

    fix for pubkey debugging

--- application/libraries/vendor/paragonie/random_compat/other/build_phar.php
+++ application/libraries/vendor/paragonie/random_compat/other/build_phar.php
@@ -29,7 +29,7 @@ rename(
  */
 if ($argc > 1) {
     if (!@is_readable($argv[1])) {
-        echo 'Could not read the private key file:', $argv[1], "\n";
+        #echo 'Could not read the private key file:', $argv[1], "\n";
         exit(255);
     }
     $pkeyFile = file_get_contents($argv[1]);
